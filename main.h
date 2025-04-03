044afa4ee2f3acd81747ebf80bf59eb9e839ff65
marcolm <thommarcolminc@gmail.com>
2025-04-03 03:26:33 +0300

    Housekeep 18th project - custom printf function (_printf)

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* includes */
+#include <stdarg.h>
+#include <stdlib.h>
+
+
+/* prototypes */
+int _putchar(char);
+int _printf(const char *format, ...);
+
+
+#endif /* MAIN_H */
